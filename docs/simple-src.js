84083555770f416c677463653e2112b25008111a
dmi3y <lapshukov@gmail.com>
2017-03-04 23:35:21 -0800

    updating simple example

--- docs/simple-src.js
+++ docs/simple-src.js
@@ -3,8 +3,28 @@
 */
 import React from 'react'
 import ReactDOM from 'react-dom'
-import {fromJS} from 'immutable'
 import Retoaster from 'retoaster'
+
+const successToast = {
+  id: 1,
+  message: 'Here we go!',
+  type: 'success'
+}
+const noteToast = {
+  id: 2,
+  message: 'Here you do!',
+  type: 'notification'
+}
+const warnToast = {
+  id: 3,
+  message: 'Here it is!',
+  type: 'warn'
+}
+const errorToast = {
+  id: 4,
+  message: 'Heres opsss!',
+  type: 'error'
+}
 
 export class SimpleExample extends React.Component {
   constructor () {
@@ -12,37 +32,22 @@ export class SimpleExample extends React.Component {
 
     this.removeToast = this.removeToast.bind(this)
     this.state = {
-      toasts: fromJS([
-        {
-          id: 0,
-          message: 'Here we go!',
-          type: 'success'
-        },
-        {
-          id: 3,
-          message: 'Here you do!',
-          type: 'notification'
-        },
-        {
-          id: 1,
-          message: 'Here it is!',
-          type: 'warn'
-        },
-        {
-          id: 2,
-          message: 'Here we ops!',
-          type: 'error'
-        }
-      ])
+      toasts: [
+        successToast,
+        noteToast,
+        warnToast,
+        errorToast
+      ]
     }
   }
   removeToast (toast) {
-    this.setState({toasts: this.state.toasts.update($$toasts => $$toasts.filter($$toast => $$toast.get('id') !== toast.id))})
+    const toasts = this.state.toasts.filter(itoast => itoast !== toast)
+    this.setState({toasts})
   }
   render () {
     return <div>
-      <h1>HERE WE ARE</h1>
-      <Retoaster toasts={this.state.toasts.toJS()} removeToast={this.removeToast} />
+      <h1>Simple Example</h1>
+      <Retoaster toasts={this.state.toasts} removeToast={this.removeToast} />
     </div>
   }
 }
